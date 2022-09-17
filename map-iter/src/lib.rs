ab6e8cccdf7047b88237779e2167f4747e1e3dd7
Bogdan Kushnir <wareverbohdan@gmail.com>
2022-09-17 22:36:10 +0300

    cleanup: Removed ToStringMapper. Renamings to a shorter names

--- map-iter/src/lib.rs
+++ map-iter/src/lib.rs
@@ -12,28 +12,6 @@ pub trait Invoke<Args>: InvokeMut<Args> {
     fn invoke(&self, args: Args) -> Self::Output;
 }
 
-pub struct ToStringMapper;
-
-impl<'a> InvokeOnce<(&'a str,)> for ToStringMapper {
-    type Output = String;
-
-    fn invoke_once(self, args: (&'a str,)) -> Self::Output {
-        String::from(args.0)
-    }
-}
-
-impl<'a> InvokeMut<(&'a str,)> for ToStringMapper {
-    fn invoke_mut(&mut self, args: (&'a str,)) -> Self::Output {
-        String::from(args.0)
-    }
-}
-
-impl<'a> Invoke<(&'a str,)> for ToStringMapper {
-    fn invoke(&self, args: (&'a str,)) -> Self::Output {
-        String::from(args.0)
-    }
-}
-
 pub struct InvokeFn<F>(F);
 
 impl<F, Arg, R> InvokeOnce<(Arg,)> for InvokeFn<F>
@@ -43,7 +21,7 @@ where
     type Output = F::Output;
 
     fn invoke_once(self, args: (Arg,)) -> Self::Output {
-        (self.0)(args.0)
+        self.0(args.0)
     }
 }
 
@@ -74,8 +52,7 @@ where
     type Output = R;
 
     fn invoke_once(self, args: (&&'a A,)) -> Self::Output {
-        let a: &'a A = args.0;
-        self.0.invoke_once((a,))
+        self.0.invoke_once((*args.0,))
     }
 }
 
@@ -84,8 +61,7 @@ where
     T: InvokeMut<(&'a A,)>,
 {
     fn invoke_mut(&mut self, args: (&&'a A,)) -> Self::Output {
-        let a: &'a A = args.0;
-        self.0.invoke_mut((a,))
+        self.0.invoke_mut((*args.0,))
     }
 }
 
@@ -94,23 +70,22 @@ where
     T: Invoke<(&'a A,)>,
 {
     fn invoke(&self, args: (&&'a A,)) -> Self::Output {
-        let a: &'a A = args.0;
-        self.0.invoke((a,))
+        self.0.invoke((*args.0,))
     }
 }
 
-pub struct MapIter<I, F> {
+pub struct Map<I, F> {
     inner: I,
     map: F,
 }
 
-impl<I, F> MapIter<I, F> {
+impl<I, F> Map<I, F> {
     pub fn new(inner: I, map: F) -> Self {
         Self { inner, map }
     }
 }
 
-impl<I, F, B> Iterator for MapIter<I, F>
+impl<I, F, B> Iterator for Map<I, F>
 where
     I: Iterator,
     F: InvokeMut<(I::Item,), Output = B>,
@@ -122,7 +97,7 @@ where
     }
 }
 
-pub struct FlatMapIter<I: Iterator, F: Invoke<(I::Item,)>>
+pub struct FlatMap<I: Iterator, F: Invoke<(I::Item,)>>
 where
     F::Output: IntoIterator,
 {
@@ -131,7 +106,7 @@ where
     f: F,
 }
 
-impl<I: Iterator, F: Invoke<(I::Item,)>> FlatMapIter<I, F>
+impl<I: Iterator, F: Invoke<(I::Item,)>> FlatMap<I, F>
 where
     F::Output: IntoIterator,
 {
@@ -144,7 +119,7 @@ where
     }
 }
 
-impl<I, F: Invoke<(I::Item,)>> Iterator for FlatMapIter<I, F>
+impl<I, F: Invoke<(I::Item,)>> Iterator for FlatMap<I, F>
 where
     I: Iterator,
     F::Output: IntoIterator,
@@ -182,12 +157,12 @@ mod tests {
 
     #[test]
     fn map_iter_should_be_iterable() {
-        let src = vec!["red", "green", "blue"];
-        let iter: MapIter<std::slice::Iter<&str>, RefArg<ToStringMapper>> =
-            MapIter::new(src.iter(), RefArg(ToStringMapper));
+        let src = ["red", "green", "blue"];
+        let iter: Map<std::slice::Iter<&str>, RefArg<InvokeFn<fn(&'static str) -> String>>> =
+            Map::new(src.iter(), RefArg(InvokeFn(String::from)));
 
         assert_eq!(
-            src.iter().map(|s| String::from(*s)).collect::<Vec<_>>(),
+            src.into_iter().map(String::from).collect::<Vec<_>>(),
             iter.collect::<Vec<_>>()
         );
     }
@@ -202,10 +177,8 @@ mod tests {
     #[test]
     fn flat_map_iter_should_be_iterable() {
         let src: [&'static str; 3] = ["red", "green", "blue"];
-        let iter: FlatMapIter<
-            std::slice::Iter<&str>,
-            RefArg<InvokeFn<fn(&str) -> std::str::Chars>>,
-        > = FlatMapIter::new(src.iter(), RefArg(InvokeFn(str::chars)));
+        let iter: FlatMap<std::slice::Iter<&str>, RefArg<InvokeFn<fn(&str) -> std::str::Chars>>> =
+            FlatMap::new(src.iter(), RefArg(InvokeFn(str::chars)));
 
         let expected = String::from("redgreenblue");
         let v = iter.collect::<String>();
